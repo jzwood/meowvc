6101b5cb1f68173e8bedaec72c9632296a27db06
Jake Wood <jwood999@apple.com>
2017-07-06 17:57:34 -0700

    new stat implementation

--- src/discretize.js
+++ src/discretize.js
@@ -1,52 +1,86 @@
 const fs = require('fs-extra')
-const readline = require('readline')
 const path = require('path')
 const crc = require('crc')
+const chalk = require('chalk')
 
 module.exports = cwd => {
 	const dotMu = '.mu'
 	const linesPath = path.join(cwd, dotMu, 'disk_mem', 'lines')
 	const filesPath = path.join(cwd, dotMu, 'disk_mem', 'files')
+	const print = {
+		'modified': str => console.log(chalk.cyan('%\t' + str)),
+		'deleted': str => console.log(chalk.red('-\t' + str)),
+		'added': str => console.log(chalk.yellow('+\t' + str))
+	}
 	const memory = new Set()
+	const modifiedFiles = new Set()
 	const outputFileQueue = []
 	const ignore_file = fs.readFileSync(path.join(cwd, dotMu, '_ignore'), 'utf8').trim().split('\n').join('|')
 	const ignore = ignore_file ? new RegExp(ignore_file) : void(0)
 
+	const hashIt = data => {
+		const h = crc.crc32(data).toString(16)
+		if (h === '0') return '00000000'
+		return h
+	}
+
 	return {
-		save(){
+		save() {
 			const tree = getTree()
 			let outputFile
-			if(outputFileQueue.length){
-				while(outputFile = outputFileQueue.pop()){
+			if (outputFileQueue.length) {
+				while (outputFile = outputFileQueue.pop()) {
 					fs.outputJsonSync(outputFile[0], outputFile[1])
 				}
 				const pointerPath = path.join(cwd, dotMu, '_pointer.json')
 				const pointer = fs.readJsonSync(pointerPath)
 				fs.outputJsonSync(path.join(cwd, dotMu, 'history', pointer.head, 'v' + pointer.branch[pointer.head]), tree)
 				pointer.branch[pointer.head]++
-				fs.outputJsonSync(pointerPath, pointer)
+					fs.outputJsonSync(pointerPath, pointer)
 				return true
 			}
 			return false
 		},
-		stat(){
-			getTree() //populates outputFileQueue
+		stat() {
+			blockify(cwd, true)
 			let outputFile
-			if(outputFileQueue.length){
-				while(outputFile = outputFileQueue.pop()){
-					console.log(outputFile)
+			if (outputFileQueue.length) {
+				const pointerPath = path.join(cwd, dotMu, '_pointer.json')
+				const pointer = fs.readJsonSync(pointerPath)
+				const currentVersion = pointer.branch[pointer.head].toString()
+
+				let previousFilepaths
+				if (currentVersion === '0') {
+					previousFilepaths = []
+				} else {
+					const lastSave = path.join(cwd, dotMu, 'history', pointer.head, 'v' + (currentVersion - 1))
+					previousFilepaths = Object.keys(lastSave.dat)
+				}
+				let current
+				while (current = previousFilepaths.pop()) {
+					if (modifiedFiles.delete(current)) {
+						print.modified(current)
+					} else {
+						print.deleted(current)
+					}
+					for (let item of modifiedFiles) {
+						print.added(item)
+					}
 				}
 			}
 		}
 	}
 
-	function getTree(){
+	function getTree() {
 		preCache()
-		return blockify(cwd)
+		return blockify(cwd, false)
 	}
 
-	function blockify(parent) {
-		const baseCase = {}
+	function blockify(parent, isStat) {
+		const baseCase = {
+			'ino': {},
+			'dat': {}
+		}
 		if (!fs.existsSync(parent)) return baseCase
 		const hashFileLookup = fs.readdirSync(parent).reduce((tree, child) => {
 			if (ignore && ignore.test(child)) return tree
@@ -57,14 +91,17 @@ module.exports = cwd => {
 			} else {
 				const childRelativePath = path.relative(cwd, childPath)
 				const status = fs.statSync(childRelativePath)
-				const inode = status.ino, size = status.size, mtime = fs._toUnixTimestamp(status.mtime)
-				const data = tree[inode] && tree[tree[inode]] || []
-				if(data[0] !== childRelativePath || data[1] !== size || data[3] !== mtime){
-					const hashsum = hashNCache(childRelativePath)
-					tree[inode] = hashsum
-					tree[hashsum] = [childRelativePath, status.size, fs._toUnixTimestamp(status.mtime)]
+				const inode = status.ino,
+					size = status.size,
+					mtime = fs._toUnixTimestamp(status.mtime)
+				const data = tree.ino[inode] && tree.dat[tree.ino[inode]] || []
+				if (data[0] !== childRelativePath || data[1] !== size || data[3] !== mtime) {
+					const hashsum = isStat ? hashOnly(childRelativePath) : hashNCache(childRelativePath)
+					tree.ino[inode] = hashsum
+					tree.dat[hashsum] = [childRelativePath, status.size, fs._toUnixTimestamp(status.mtime)]
+					modifiedFiles.add(childRelativePath)
 				}
-				// else? no changes required!
+				// ELSE DO NOTHING
 			}
 			return tree
 		}, baseCase)
@@ -87,23 +124,23 @@ module.exports = cwd => {
 		})
 	}
 
+	function hashOnly(fpath){
+		const file = fs.readFileSync(fpath, 'utf8')
+		return hashIt(file)
+	}
+
 	function hashNCache(fpath) {
-		const hashIt = data => {
-			const h = crc.crc32(data).toString(16)
-			if(h === '0') return '00000000'
-			return h
-		}
 		const isUncached = hash => !(memory.has(hash))
 		const cacheIt = data => {
 			memory.add(data)
 		}
-		const insert = (string, index, substr) => string.slice(0, index) + substr + string.slice(index)
 
 		const file = fs.readFileSync(fpath, 'utf8')
 		const fileHash = hashIt(file)
 
 		if (isUncached(fileHash)) {
 			cacheIt(fileHash)
+			const insert = (string, index, substr) => string.slice(0, index) + substr + string.slice(index)
 			const hashes = file.split('\n').map(line => {
 				const lineHash = hashIt(line)
 				if (isUncached(lineHash)) {
@@ -112,7 +149,7 @@ module.exports = cwd => {
 				}
 				return lineHash
 			})
-			outputFileQueue.push([path.join(filesPath, insert(fileHash, 2, '/')), hashes])
+			outputFileQueue.push([path.join(filesPath, insert(fileHash, 2, '/')), hashes, fpath])
 		}
 		return fileHash
 	}
