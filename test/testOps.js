5e2545abf54275c6064d055714f2d0627b9c15dc
Jake Wood <jwood999@apple.com>
2017-09-11 11:52:23 -0700

    fixes one line bug

--- test/testOps.js
+++ test/testOps.js
@@ -24,8 +24,13 @@ function setupTest(){
   process.chdir(cwdTemp)
 }
 
-function randomAscii(asciiLow, asciiHi, numOfChars){
-  return String.fromCharCode.apply(undefined,Array(numOfChars).fill(0).map(i => asciiLow + ~~(Math.random() * (asciiHi - asciiLow))))
+function insert(string, index, substr){
+  return string.slice(0, index) + substr + string.slice(index)
+}
+
+function randomAscii(validIndices, numOfChars){
+  const getValidIndex = () => validIndices[~~(validIndices.length * Math.random())]
+  return String.fromCharCode.apply(undefined,Array(numOfChars).fill(0).map(getValidIndex))
 }
 
 function newline(){
@@ -49,12 +54,30 @@ function rename(fpath){
 }
 
 function makeWord(){
-  return randomAscii(97,122,~~(3+Math.random()*3))
+  return randomAscii(range(97,122) ,~~(3+Math.random()*3))
+}
+
+function range(lo, hi){
+  return Array(hi - lo).fill(0).map((_, i) => i + lo)
+}
+
+function safeInds(newLineFreq=0.1){
+  const nls = Array(~~(newLineFreq * (127-32))).fill(10)
+  return range(32,127).concat(nls)
+}
+
+function allInds(){
+  return range(0,256)
+}
+
+function coinFlip(){
+  return (+new Date()) % 2
 }
 
 function addRandomFile(depth=1){
   const fpath = path.join(...Array(Math.max(1, depth)).fill('').map(makeWord)) + '.txt'
-  let data = randomAscii(0,255,~~(Math.random() * 5000))
+  const inds = coinFlip() ? allInds() : safeInds()
+  let data = randomAscii(inds, ~~(Math.random() * 1000))
   fs.outputFileSync(fpath, data)
   data = fs.readFileSync(fpath)
   console.info(chalk.yellow(`+\t${fpath}`))
@@ -62,7 +85,8 @@ function addRandomFile(depth=1){
 }
 
 function modFile(fpath){
-  const data = randomAscii(0,255,~~(Math.random() * 5000))
+  const inds = coinFlip ? allInds() : safeInds()
+  const data = randomAscii(inds,~~(Math.random() * 1000))
   fs.outputFileSync(fpath, data)
   console.info(chalk.cyan(`%\t${fpath}`))
 }
