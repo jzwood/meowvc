4f5610cee9a1386316c61a2cd3473821e94c3f12
Jake Wood <jwood999@apple.com>
2017-09-25 08:56:46 -0700

    added svg to readme

--- src/arguments/save.js
+++ src/arguments/save.js
@@ -7,7 +7,7 @@ const core = require('../core')()
 
 module.exports = function save(i, args){
   const msg = args[i + 1]
-  if(typeof message === 'string' && message.length > 0){
+  if(typeof msg === 'string' && msg.length > 0){
     core.save(null, {msg})
   }else{
     console.log(chalk.red('save expects a message, e.g. ') + chalk.inverse('$ mu save "fixes punctuation errors"'))
