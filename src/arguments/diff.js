1c23be4996842c9ed7c98d3a1366e4052f9f65b8
Jake Wood <jwood999@apple.com>
2017-09-30 15:11:47 -0700

    adds diff basic outline

--- src/arguments/diff.js
+++ src/arguments/diff.js
@@ -5,6 +5,8 @@ const eol = require('os').EOL
 const pointerOps = require('../modules/pointerOps')
 const muOps = require('../modules/muOps')
 
+const minimumEditDistance = require('minimum-edit-distance')
+
 /*********
 *  DIFF  *
 *********/
@@ -13,7 +15,22 @@ module.exports = function diff(i, args){
   const [p1, p2=10] = args.slice(i + 1)
 
   console.info(p1,p2)
-  return 0
+  // return 0
+
+  const smaller = Math.min(p1.length, p2.length)
+  const diffSize = 1000
+
+  let index
+  for(index=0; index<smaller; index+=diffSize){
+    let difference = minimumEditDistance.diff(p1, p2)
+    let backtrace = difference.backtrace
+    displayDiff(p1, backtrace)
+  }
+  if(p1.length > p2.length){
+    displayRemoved(p1.slice(i, p1.length-1))
+  }else{
+    displayAdded(p2.slice(i, p2.length-1))
+  }
 
   let ancestor
   if (muOps.repoPath){
