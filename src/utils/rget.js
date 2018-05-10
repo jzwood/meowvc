14ffc9a452074d414bf8d3308a850f151ee4c303
Jake Wood <jwood999@apple.com>
2018-05-10 09:35:58 -0700

    rget works

--- src/utils/rget.js
+++ src/utils/rget.js
@@ -1,25 +1,27 @@
+const fs = require('fs-extra')
+const path = require('path')
 /**
  * @description - recursively finds all files
  * @returns - promise for array of files
  */
-module.exports = async (root, igorePattern) => {
+module.exports = (root, ignorePattern) => {
   const rget = async (parent) => {
     return (await fs.readdir(parent))
-      .reduce(async (files, child, index, ls) => {
+      .reduce(async (pfiles, child, index, ls) => {
+        const files = await pfiles
         if (!ignorePattern.test(child)) {
           const childpath = path.join(parent, child)
           const status = await fs.stat(childpath)
           if (status.isDirectory()) {
-            return files.concat(rget(childpath))
+            return rget(childpath)
           }
           if (status.isFile()) {
-            const relpath = path.relative(treeRoot, childpath)
-            return files.concat({childPath, relpath, status})
+            const relpath = path.relative(root, childpath)
+            return files.concat({ childpath, relpath, status })
           }
-          return files
         }
-      }, [])
+        return files
+      }, Promise.resolve([]))
   }
   return rget(root)
 }
-
--- test/unit/rget.js
+++ test/unit/rget.js
@@ -1,9 +1,11 @@
 const test = require('ava')
 const chalk = require('chalk')
+const path = require('path')
 
 const rget = require('../../src/utils/rget')
 
 test('rget', async t => {
-  console.log(rget)
+  var a = (await rget(path.join(process.cwd(),'src'), new RegExp('^\\.')))
+  console.log(a)
   t.pass()
 })
